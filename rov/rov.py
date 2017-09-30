43e5de8ea6389a752a578ed5cf9c9171bf829c79
Ben Maxfield <bmaxfie@purdue.edu>
2017-09-30 15:46:35 -0400

    Removed everything from rov.py except for sensors and basic thrusters.

--- rov/rov.py
+++ rov/rov.py
@@ -7,15 +7,9 @@ from time import time, sleep
 
 
 from sensors import Pressure, IMU
-from camera import Cameras
 
 from hardware.motor_control import MotorControl
 
-from thrusters.Control import ThrusterControl
-from thrusters.hardware.PWM_Control import Thrusters
-from thrusters.mapper.Simple import Mapper
-
-from tools import Claw, ValveTurner, FountainTool
 
 
 class ROV(object):
@@ -54,37 +48,6 @@ class ROV(object):
             frequency=47
         )
 
-        self.thrusters = Thrusters(
-            self.motor_control,
-            [7, 4, 6, 5, 10, 0, 11, 1]
-        )
-
-        self.thrust_mapper = Mapper()
-
-        self.thruster_control = ThrusterControl(
-            self.thrusters,
-            self.thrust_mapper,
-            num_thrusters=8,
-            ramp=True,
-            max_ramp=0.03
-        )
-
-        self.valve_turner = ValveTurner(
-            self.motor_control,
-            pin=8
-        )
-
-        self.claw_status = False
-        self.claw = Claw(
-            self.motor_control,
-            pin=3
-        )
-
-        self.fountain_tool = FountainTool(
-            self.motor_control,
-            pin=2
-        )
-
         #""" Disabled until hardware is done and sw is tested
         # self.IMU = IMU()
         # self.pressure = Pressure()
@@ -103,30 +66,6 @@ class ROV(object):
             df = self.dearflask
             print df
 
-            self.thruster_control.update(**df['thrusters'])
-
-            self.valve_turner.update(df['valve_turner']['power'])
-            self.fountain_tool.update(df['fountain_tool']['power'])
-
-            # if 'claw' is powered, turn off, else if last status was off but current status is on then let it be on (turn ramping off).
-            # This assumes an on period per button press of about 10ms.
-            if self.claw_status == True and df['claw']['power'] != 0:
-                claw_power = (df['claw']['power'])
-            else:
-                claw_power = 0.0
-
-            self.claw.update(claw_power)
-            if df['claw']['power'] != 0:
-                self.claw_status = True
-            else:
-                self.claw_status = False
-
-            #cam = df['cameras']
-            #for cam in df['cameras']:
-            #    if (cam['status'] == 0):
-            #        self.cameras.kill(cam['port'])
-            #    if (cam['status'] == 1):
-            #        self.cameras.start(cam['port'])
 
             """ Disabled until hardware is done and sw is tested
             self.pressure.update()
@@ -144,8 +83,6 @@ class ROV(object):
         self.last_update = time()
 
         self.dearclient['last_update'] = self.last_update
-        self.dearclient['thrusters'] = self.thruster_control.data
-        #self.dearclient['cameras'] = self.cameras.status()
 
         with self._data_lock:
             self._data['dearclient'] = self.dearclient
