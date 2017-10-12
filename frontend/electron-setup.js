57db4a0b81347e033eb6a0088af1f66d2496fc38
Ya-WenChu <chu110@purdue.edu>
2017-10-12 19:40:35 -0400

    Fixed electron-setup.js

--- frontend/electron-setup.js
+++ frontend/electron-setup.js
@@ -12,7 +12,7 @@ function createWindow () {
 
   // and load the index.html of the app.
   win.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
+    pathname: path.join('/Users/student/GitHub/XX-Core/frontend', 'index.html'),
     protocol: 'file:',
     slashes: true
   }))
@@ -56,20 +56,24 @@ app.on('activate', () => {
 
 
 var spawn = require('child_process').spawn,
-    py    = spawn('python', ['print.py']),
+    py    = spawn('python', ['./src/print.py']),
     data = [1,2,3,4,5,6,7,8,9],
     dataString = '';
 
   /*Here we are saying that every time our node application receives data from the python process output stream(on 'data'), we want to convert that received data into a string and append it to the overall dataString.*/
+
 py.stdout.on('data', function(data){
   dataString = data.toString();
 });
 
 /*Once the stream is done (on 'end') we want to simply log the received data to the console.*/
+
 py.stdout.on('end', function(){
   console.log('Sum of numbers=',dataString);
 });
+
 /*We have to stringify the data first otherwise our python process wont recognize it*/
+
 py.stdin.write(JSON.stringify(data));
 
 py.stdin.end();
