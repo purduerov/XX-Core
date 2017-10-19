337c1f6d0cb4f0a70d64da455ba440d130df9458
isibley765 <isibley@purdue.edu>
2017-10-18 20:38:52 -0400

    Setting up packed transfers based off of last year

--- frontend/electron-setup.js
+++ frontend/electron-setup.js
@@ -8,11 +8,11 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600})
+  win = new BrowserWindow({width: 1600, height: 1200})
 
   // and load the index.html of the app.
   win.loadURL(url.format({
-    pathname: path.join('/Users/student/GitHub/XX-Core/frontend', 'index.html'),
+    pathname: path.join(__dirname, 'main.html'),
     protocol: 'file:',
     slashes: true
   }))
