2b01b5977f9eaa9f4a23df4ba63f2915536d219e
rmcbee <rmcbee@purdue.edu>
2017-10-21 12:54:32 -0400

    swithching branches

---
+++ rov/movement/tests/test_mapper.py
@@ -0,0 +1,13 @@
+"""
+Tests the thrust mapper to verify that the thrust mapper
+outputs thruster values that appear to be reasonable.
+"""
+
+import pytest
+import rov.movement.mapper.Simple as Mapper
+
+
+def test_zero_output_with_zero_input():
+    """Tests that the thrust mapper outputs all zeros when inputted with all zeros"""
+    pass
+
