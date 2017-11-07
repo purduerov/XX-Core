eb11e73d93893f8c470d1d0dcc1ed41b3a366257
kaylwin <kaylwin@purdue.edu>
2017-11-07 12:34:53 -0500

    Create initial files for convolution test

---
+++ vision/convolutionTest.py
@@ -0,0 +1,28 @@
+import cv2
+from settings import *
+
+# Parameters assumed to be defined in settings.py
+# After -- is example definition
+#================================================
+# videofilename -- "../calib1.mp4"
+# vidStartTime  --  31
+# vidStopTime  --   41
+#================================================
+
+
+# Create video feed
+
+
+
+#Generate Gaussian Pyramid
+
+
+
+#Locate object in frame
+
+
+
+#Display result
+
+
+
