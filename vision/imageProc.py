bc679691cbe7e656524d28045da3fd169c83339c
joey9z <jdanial@purdue.edu>
2017-11-18 14:47:41 -0500

    implemented find scale and adjustScale functions

--- vision/imageProc.py
+++ vision/imageProc.py
@@ -4,11 +4,31 @@
 template size to largest template size and will terminate pre-maturely if
 the match correlation decreases continuously with a size increase"""
 def findScale(inputFrame, template):
-    pass
-
+    maxMatchVal = 0
+    maxMatchLoc = (0, 0)
+    maxIndex = currIndex
+    for idx, template in enumerate(templates):       
+        res = cv2.matchTemplate(frame, template, cv2.TM_CCORR_NORMED)
+        minVal, maxVal, minLoc, maxLoc = cv2.minMaxLoc(res)
+        if maxVal > maxMatchVal:
+            maxMatchVal = maxVal
+            maxMatchLoc = maxLoc
+            maxIndex = idx
+    return maxLoc, maxIndex
 
 """ Searches above and below current index to see which template matches
-the input frame better and returns the center coordinate, the new best index
+the input frame better and returns the top left coordinate, the new best index
 """
 def adjustScale(currentFrame, currIndex, templates):
-    pass
+    maxMatchVal = 0
+    maxMatchLoc = (0, 0)
+    maxIndex = currIndex
+    for idx in range(currIndex-1, currIndex+2):
+        if idx >=0 and idx < len(templates):        
+            res = cv2.matchTemplate(frame, templates[currIndex], cv2.TM_CCORR_NORMED)
+            minVal, maxVal, minLoc, maxLoc = cv2.minMaxLoc(res)
+            if maxVal > maxMatchVal:
+                maxMatchVal = maxVal
+                maxMatchLoc = maxLoc
+                maxIndex = idx
+    return maxLoc, maxIndex
