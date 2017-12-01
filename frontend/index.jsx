a50fe746daef9cf09a361d0ab8c5b6104604e4aa
Ian Sibley <isibley@purdue.edu>
2017-12-01 01:42:45 +0000

    Should be pull request ready, compiler works

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,21 +1,11 @@
 import React from 'react';
 import {render} from 'react-dom';
-import AwesomeComponent from './src/components/AwesomeComponent.jsx';
-//import NewComponent from './src/components/NewComponent.jsx';
-//<NewComponent />
-/*
-class App extends React.Component {
-  render () {
-    return <p> Hello React, how do the do?!</p>;
-  }
-}
-*/
+
 class App extends React.Component {
   render () {
     return (
       <div>
         <p> Hello React!</p>
-        <AwesomeComponent />
       </div>
     );
   }
