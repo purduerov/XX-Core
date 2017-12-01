38c1525853cd449939c603b8832baf6acc047188
Ian Sibley <isibley@purdue.edu>
2017-12-01 00:54:25 +0000

    Fixed, the script file needed to go after the dom element was loaded

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -5,20 +5,18 @@ var BUILD_DIR = path.resolve(__dirname, 'src/elec_finals/');
 var APP_DIR = __dirname;
 
 var config = {
-  entry: APP_DIR + '/index.jsx',
-  output: {
-    path: BUILD_DIR,
-    filename: 'build.js'
-  },
-  module : {
-    loaders : [
-      {
-        test : /\.jsx?/,
-        include : APP_DIR,
-        loader : 'babel-loader'
-      }
-    ]
-  }
+    entry: APP_DIR + '/index.jsx',
+    output: {
+        path: BUILD_DIR,
+        filename: '.build.js'
+    },
+    module: {
+        loaders: [{
+            test: /\.jsx?/,
+            include: APP_DIR,
+            loader: 'babel-loader'
+        }]
+    }
 };
 
 module.exports = config;
