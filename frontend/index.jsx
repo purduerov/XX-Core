cc7e597c7539031c7fbd34a66523916d80f50a03
Ian Sibley <isibley@purdue.edu>
2017-12-02 23:25:14 +0000

    Compiler carries styling over

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {render} from 'react-dom';
+import Titlebar from './src/components/Titlebar.jsx';
 
 class App extends React.Component {
   render () {
     return (
       <div>
+      <Titlebar/>
         <p> Hello React!</p>
       </div>
     );
