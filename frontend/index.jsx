e8d4bd6bca9147d5152751b8ded8070f56d078ad
Ian Sibley <isibley@purdue.edu>
2017-12-27 04:20:07 +0000

    Randomized, but dynamically updates

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -7,7 +7,17 @@ import Cam_view from './src/components/Cam_View.jsx';
 import Titlebar from './src/components/Titlebar.jsx';
 import ThrusterInfo from './src/components/ThrusterInfo.jsx';
 
+//var packets = require("./src/packets.js");
+let socketHost = `ws://raspberrypi.local:5000`;
+let socket = io.connect(socketHost, {transports: ['websocket']});
+let {shell, app, ipcRenderer} = window.require('electron');
+
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = require("./src/packets.js"); //= $.extend(true, {}, packets);
+  }
+
   render () {
     return (
       <div className="main">
@@ -19,11 +29,11 @@ class App extends React.Component {
               </div>
               <div className="data-width full-height">
                   <div className="data-column">
+                    <Card title="Thrusters">
+                      <ThrusterInfo thrusters={this.state.dearclient.thrusters}/>
+                    </Card>
                   </div>
                   <div className="data-column">
-                    <Card title="Thrusters">
-                      <ThrusterInfo thrusters={packet.dearclient.thrusters}/>
-                    </Card>
                   </div>
                   <div className="data-column">
                   </div>
@@ -31,6 +41,40 @@ class App extends React.Component {
           </div>
       </div>
     );
+  }
+
+  componentDidMount() {
+    var that = this;
+    window.react = this;
+    setInterval(function() {
+      let all = that.state;
+      all.dearclient.thrusters.forEach(function(key, i, arr) {
+        arr[i] = Math.random();
+      });
+
+      that.setState(
+        all
+      );
+    }, 3000);
+
+    // upon new data, save it locally
+    socket.on("dearclient", function(data) {    //Updates the data sent back from the server
+        let all = that.state;
+        all.dearclient = data;
+        that.setState(
+          all
+        );
+    });
+
+    // request new data
+    setInterval(() => {
+        socket.emit("dearclient");
+    }, 50);
+
+    // send new data
+    setInterval(() => {             //Sends a message down to the server with updated surface info
+        socket.emit("dearflask", that.state.dearflask);
+    }, 50);
   }
 }
 
