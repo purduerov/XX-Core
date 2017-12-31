72d00bbf1ee5980542feb4b51e8f37d10b119aed
Viktoryia <vzhuleva@purdue.edu>
2017-12-31 18:18:49 -0500

    got the logic for the activate/deactivate

--- rov/movement/controls/control_handler.py
+++ rov/movement/controls/control_handler.py
@@ -8,9 +8,7 @@ class Master_Control_Handler():
     #compare the activation logic
 
     def __init__(self, desired_thrust_in, frozen_in): #refer to the XX-Core/frontend/src/packets.js
-        self.desired_thrust_in = desired_thrust_in
-        self.frozen_in = frozen_in
-        self.dof_control = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+        self.dof_control = desired_thrust_in
         self.dimension_lock = [False, False, False, False, False, False]
         self.dof_names = ['x', 'y', 'z', 'roll', 'pitch', 'yaw']
         self.xfreeze = ControlAlgorithm('x')
@@ -22,26 +20,56 @@ class Master_Control_Handler():
 
 
 
-    def master(self): # sort of "main" function
+    def master(self, desired_thrust_in, frozen_in): # "main" control handler
         
         #TODO: the activation
         #if prev_activate[i] = 1 and current is 0 then we gotta deactivate it
-        
-        
-        
+        i = 0
+        while(i < 6):
+            if(prev_activate[i] == False && frozen_in[i] == True):
+                if (i==0):
+                    self.xfreeze.activate(desired_thrust_in[i])
+                if (i==1):
+                    self.yfreeze.activate(desired_thrust_in[i])
+                if (i==2):
+                    self.zfreeze.activate(desired_thrust_in[i])
+                if (i==3):
+                    self.rollfreeze.activate(desired_thrust_in[i])
+                if (i==4):
+                    self.pitchfreeze.activate(desired_thrust_in[i])
+                if (i==5):
+                    self.yawfreeze.activate(desired_thrust_in[i])
+            else if (prev_activate[i] == True && frozen_in[i] == False):
+                if (i==0):
+                    self.xfreeze.deactivate
+                if (i==1):
+                    self.yfreeze.deactivate
+                if (i==2):
+                    self.zfreeze.deactivate
+                if (i==3):
+                    self.rollfreeze.deactivate
+                if (i==4):
+                    self.pitchfreeze.deactivate
+                if (i==5):
+                    self.yawfreeze.deactivate
+            i+=1
+ 
         i = 0
         while (i < 6):
-            if (self.frozen_in[i] == True):
+            if (frozen_in[i] == True):
                 if (i==0):
-                    self.dof_control[0] = self.xfreeze
+                    self.dof_control[0] = self.xfreeze.calculate
                 if (i==1):
-                    self.dof_control[1] = self.yfreeze
+                    self.dof_control[1] = self.yfreeze.calculate
                 if (i==2):
-                    self.dof_control[2] = self.zfreeze
+                    self.dof_control[2] = self.zfreeze.calculate
                 if (i==3):
-                    self.dof_control[3] = self.rollfreeze
+                    self.dof_control[3] = self.rollfreeze.calculate
                 if (i==4):
-                    self.dof_control[4] = self.pitchfreeze
+                    self.dof_control[4] = self.pitchfreeze.calculate
                 if (i==5):
-                    self.dof_control[5] = self.yawfreeze
-            i++
+                    self.dof_control[5] = self.yawfreeze.calculate
+            i += 1
+            
+            
+        prev_activate = frozen_in
