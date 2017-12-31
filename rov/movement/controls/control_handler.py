ad82fc464954327528626d7ec9df15acdc038805
Viktoryia Zhuleva <vzhuleva@purdue.edu>
2017-12-31 17:40:17 -0500

    changes to control handler

--- rov/movement/controls/control_handler.py
+++ rov/movement/controls/control_handler.py
@@ -1,7 +1,22 @@
+from rov.movement.controls.Control_Algorithm.py import ControlAlgorithm
+
 class Master_Control_Handler():
     #global variables
     prev_activate = [0, 0, 0, 0, 0, 0]
 
-    def __init__(self, thrusters, sensor_ref):
+    def __init__(self, desired_thrust_in, frozen_in): #refer to the XX-Core/frontend/src/packets.js
+        self.desired_thrust_in = desired_thrust_in
+        self.frozen_in = frozen_in
         self.dof_control = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
         self.dimension_lock = [False, False, False, False, False, False]
+
+
+
+    def master(self): # sort of "main" function
+        while (i < 6):
+            if (self.frozen_in[i] == True):
+                if (i==0):
+                    #what to put here?
+
+
+            i++
