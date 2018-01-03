eddc53f80ad9981a0c8f17994f639a44c8a39da9
lee2670 <lee2670@purdue.edu>
2018-01-02 22:13:35 -0500

    add four constants: POWER_THRESH, IND_THRESH, TOTAL_CHANGE_THRESH, and IND_CHANGE_THRESH

--- rov/init_hw_constants.py
+++ rov/init_hw_constants.py
@@ -3,3 +3,7 @@ ZERO_POWER = 305  # Value to set thrusters to 0 power
 NEG_MAX_POWER = 222  # Value to set thrusters to max negative power
 POS_MAX_POWER = 388  # Value to set thrusters to max positive power
 FREQUENCY = 47  # Frequency at which the i2c to pwm will be updated
+POWER_THRESH = 1000 # arbitrary power threshold value
+IND_THRESH = 300 # arbitrary individual thruster threshold value
+TOTAL_CHANGE_THRESH = 1000 # arbitrary total change in thrusters threshold value
+IND_CHANGE_THRESH = 1000 # arbitrary individual change in a thruster threshold value
