a210562e04b644c781e6babc46847b04a1be0966
anotheroctopus <li1735@purdue.edu>
2018-01-03 20:50:05 -0800

    moved logtest into pakfront.go

--- pakfront/pakfront.go
+++ pakfront/pakfront.go
@@ -7,9 +7,16 @@ import (
 	"fmt"
 	"net"
 	"net/http"
-	"imbuff"
+	"rovproxy/imbuff"
 	"time"
+	"log"
 	"encoding/binary"
+	"runtime"
+//	"strconv"
+
+	"github.com/graarh/golang-socketio"
+	"github.com/graarh/golang-socketio/transport"
+	"github.com/googollee/go-socket.io"
 )
 
 func check(e error) {
@@ -46,7 +53,50 @@ func tcprec(port string, size int) (r int, b []byte) {
 	}
 	return tread-8, buf[8:]
 }
+func sockiopxy(rovIP string, rovPort int, clientPort string){
+		runtime.GOMAXPROCS(runtime.NumCPU())
 
+	pxyToROV, err := gosocketio.Dial(
+		gosocketio.GetUrl(rovIP, rovPort, false),
+		transport.GetDefaultWebsocketTransport())
+	check(err)
+
+	pxyToClient, err := socketio.NewServer(nil)
+	check(err)
+
+	pxyToClient.On("connection", func(so socketio.Socket) {
+		pxyToROV.On("dearflask", func(c *gosocketio.Channel, msg string) string {
+			so.Emit("dearflask")
+			return "Done"
+		})
+		pxyToROV.On("dearclient", func(c *gosocketio.Channel, msg string) string {
+			so.Emit("dearclient",msg)
+			return "Done"
+		})
+
+		so.On("dearclient", func(msg string) {
+			pxyToROV.Emit("dearclient","")
+		})
+
+		so.On("dearflask", func(msg string) {
+			pxyToROV.Emit("dearflask", msg)
+		})
+
+
+		so.On("disconnection", func() {
+		})
+	})
+
+	pxyToClient.On("error", func(so socketio.Socket, err error) {
+		log.Println("error:", err)
+	})
+
+	http.Handle("/socket.io/", pxyToClient)
+	http.Handle("/", http.FileServer(http.Dir("./asset")))
+	log.Println("Serving at localhost:5001...")
+	log.Fatal(http.ListenAndServe(clientPort, nil))
+	return
+}
 //main: where the magic:the gathering happens
 func main() {
 	// fmt.Println is a very complicated function, and its depth and complexity can not be understated. Moreover, the context in which it is called multiplies its importance factorially, further growing its need. I recommend you sit down, get a big cup of warm, heavily caffinated, tea, and consider both the implication of this function, as well as what it means to you as not only a coder, but a person and a woman.
@@ -61,7 +111,7 @@ func main() {
 	//launch the server on a goroutine
 	//go http.ListenAndServe(":1945", http.HandlerFunc(chanwrite1.Streamwrite))
 	go http.ListenAndServe(":1945", http.HandlerFunc(chanwrite1.Streamwrite))
-
+	go sockiopxy("localhost",5000,":5001")
 	//constantly wait for data to come in from the port 1918, and load it when it comes in
 	go func() {
 		for {
