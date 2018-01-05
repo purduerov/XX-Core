43d61f07dd18ef9788a1f4c958365cd82cd8c5b4
anotheroctopus <li1735@purdue.edu>
2018-01-04 17:37:30 -0800

    goinstall works

--- frontend/proxyinstall.sh
+++ frontend/proxyinstall.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+pxydir="../pakfront"
 sudo apt-get install -y golang 
 go get github.com/graarh/golang-socketio
 go get github.com/googollee/go-socket.io
-go build ../pakfront/stintotcp.go 
-go build ../pakfront/tcptostdin.go 
-go build ../pakfront/pakfront.go ../pakfront/imgbuffer.go
+go build $pxydir/stintotcp.go 
+go build $pxydir/tcptostdin.go 
+go build $pxydir/pakfront.go $pxydir/imgbuffer.go
 mv stintotcp bin/
 mv tcptostdin bin/
 mv pakfront bin/
+cp $pxydir/CV/* bin/
+cp $pxydir/CVhandles.py bin/
