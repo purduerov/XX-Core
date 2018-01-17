ddbc18203cbff2ae4279052b5c9a2a1e0145f9c9
Tobi Ola <oluwatobi.ola@gmail.com>
2018-01-17 13:19:09 -0500

    Finished TODOs, simplified control handler, and added further description of how to use this class

--- rov/movement/controls/control_handler.py
+++ rov/movement/controls/control_handler.py
@@ -4,6 +4,7 @@ class Master_Control_Handler():
     #figure out the activation logic
     #compare the activation logic
 
+    #TODO: add sensor as input to the contorl algorithms
     def __init__(self, desired_thrust_in, frozen_in): #refer to the XX-Core/frontend/src/packets.js
         self.dof_control = desired_thrust_in
         self.dimension_lock = [False, False, False, False, False, False]
@@ -11,24 +12,17 @@ class Master_Control_Handler():
         self.freeze = [ControlAlgorithm('x'), ControlAlgorithm('y'), ControlAlgorithm('z'), ControlAlgorithm('roll'), ControlAlgorithm('pitch'), ControlAlgorithm('yaw')]
         self.prev_activate = [0, 0, 0, 0, 0, 0]
 
-
     def master(self, desired_thrust_in, frozen_in): # "main" control handler
-
         # axis freeze activation:
-        # TODO: Tobi: simplify code to check prev_activate != frozen_in, then 'toggle' activation.
-
         for i in range(6):
-	    # check if it was previously frozen adn currently not
-            if(self.prev_activate[i] == False and frozen_in[i] == True):
-                self.freeze[i].activate()
-	    # check if it was previously not frozen and currently is
-            else if (self.prev_activate[i] == True and frozen_in[i] == False):
-                self.freeze[i].deactivate()
+            # check if frozen control was toggled
+            if self.prev_activate[i] != frozen_in[i]:
+                self.freeze[i].toggle() 
 
         # Run the currently activated frozen axes:
-
         for i in range(6):
-            if (frozen_in[i] == True): #if the dof is frozen - calculate the adjustment
+            # if the dof is frozen - calculate the adjustment
+            if frozen_in[i] == True:
                 self.dof_control[i] = self.freeze[i].calculate()[i]
 
         self.prev_activate = frozen_in
