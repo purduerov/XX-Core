982a467ac5239a740f114a209b15ba2d490f0527
Tobi Ola <oluwatobi.ola@gmail.com>
2018-01-23 21:29:03 -0500

    updated README and how to use

--- rov/movement/controls/control_handler.py
+++ rov/movement/controls/control_handler.py
@@ -1,15 +1,26 @@
 from Control_Algorithm import ControlAlgorithm
 
+# Control Handler
+# README:
+#   This control handler takes in the users input and frozen dof's the create a new output for the thrusters
+#   This is initialized with which variables should be frozen
+#   This is updated with the desired thrust with 6 degrees and which variables should be frozen
+#   This returns the new desired thrust for the thrust mapper and ramper
+#   This is to allows the ROV to lock a certain position in the water if needed
+# How to use:
+#   1. handler = Master_Control_Handler([0,0,1,0,0,0], sensors)
+#   2. handler.master()
+#   -   This function returns the new thruster input ex: [0,1,0.4,-0.3,0]
+
 class Master_Control_Handler():
     # figure out the activation logic
     # compare the activation logic
-    
-    # TODO: create a read me explaining this and showing how to use.
     def __init__(self, frozen_in, sensors): #refer to the XX-Core/frontend/src/packets.js
         #dont think this is needed because it directly go inst o the control algorithms
         #self.sensors = sensors
         self.dof_control = [0,0,0,0,0,0]
-        self.dimension_lock = [False, False, False, False, False, False]
+        # not sure where this is used or if needed
+        # self.dimension_lock = [False, False, False, False, False, False]
         self.dof_names = ['x', 'y', 'z', 'roll', 'pitch', 'yaw']
         self.freeze = [ControlAlgorithm('x', sensors['imu']), ControlAlgorithm('y', sensors['imu']), ControlAlgorithm('z', sensors['pressure']), ControlAlgorithm('roll', sensors['imu']), ControlAlgorithm('pitch', sensors['imu']), ControlAlgorithm('yaw', sensors['imu'])]
         self.prev_activate = [0, 0, 0, 0, 0, 0]
@@ -25,8 +36,7 @@ class Master_Control_Handler():
             if self.prev_activate[i] != frozen_in[i]:
                 self.freeze[i].toggle()
 
-        # Run the currently activated frozen axes:
-
+        # Run the currently activated frozen axes
         for i in range(6):
             # if the dof is frozen - calculate the adjustment
             if frozen_in[i] == True:
