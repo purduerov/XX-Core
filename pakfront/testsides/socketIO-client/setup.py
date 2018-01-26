0e9ee349c0792b0281337f2809958660630d2429
anotheroctopus <li1735@purdue.edu>
2018-01-26 09:00:01 -0500

    added wrong package

---
+++ pakfront/testsides/socketIO-client/setup.py
@@ -0,0 +1,39 @@
+import io
+from os.path import abspath, dirname, join
+from setuptools import find_packages, setup
+
+
+HERE = dirname(abspath(__file__))
+LOAD_TEXT = lambda name: io.open(join(HERE, name), encoding='UTF-8').read()
+DESCRIPTION = '\n\n'.join(LOAD_TEXT(_) for _ in [
+    'README.rst',
+    'CHANGES.rst',
+])
+setup(
+    name='socketIO-client',
+    version='0.7.2',
+    description='A socket.io client library',
+    long_description=DESCRIPTION,
+    license='MIT',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'License :: OSI Approved :: MIT License',
+        'Development Status :: 5 - Production/Stable',
+    ],
+    keywords='socket.io node.js',
+    author='Roy Hyunjin Han',
+    author_email='rhh@crosscompute.com',
+    url='https://github.com/invisibleroads/socketIO-client',
+    install_requires=[
+        'requests>=2.7.0',
+        'six',
+        'websocket-client',
+    ],
+    tests_require=[
+        'nose',
+        'coverage',
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False)
