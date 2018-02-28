67c8a3c0c7c3a7ac95e72166a97a8f39a1ae4211
Youngsik Yoon <yoon161@purdue.edu>
2018-02-28 18:38:20 -0500

    proxy_install merge

--- surface/pakfront/proxyinstall.sh
+++ surface/pakfront/proxyinstall.sh
@@ -7,10 +7,10 @@ go get github.com/googollee/go-socket.io
 go build $pxydir/stintotcp.go 
 go build $pxydir/tcptostdin.go 
 go build -o $outname $pxydir/pakfront.go $pxydir/imgbuffer.go $pxydir/sockiopxy.go $pxydir/CVdata.go $pxydir/transparent.go $pxydir/logger.go
-mv stintotcp pakfront/bin/
-mv tcptostdin pakfront/bin/
-mv $outname pakfront/bin/
+mv stintotcp bin/
+mv tcptostdin bin/
+mv $outname bin/
 cp -r $pxydir/* bin/
 cp -r $pxydir/proxyconfig.json .
-cp $pxydir/CVhandles.py pakfront/bin/
-sudo chmod -R 770 pakfront/bin/*
+cp $pxydir/CVhandles.py bin/
+sudo chmod -R 770 bin/*
