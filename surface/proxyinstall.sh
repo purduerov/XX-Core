b917f8df98f790c37685b3ffb079825ce586286e
Youngsik Yoon <yoon161@purdue.edu>
2018-02-28 21:25:58 -0500

    Resolved merge conflicts in proxyinstall.sh

--- surface/proxyinstall.sh
+++ surface/proxyinstall.sh
@@ -7,7 +7,6 @@ go get github.com/googollee/go-socket.io
 go build $pxydir/stintotcp.go 
 go build $pxydir/tcptostdin.go 
 go build -o $outname $pxydir/pakfront.go $pxydir/imgbuffer.go $pxydir/sockiopxy.go $pxydir/CVdata.go $pxydir/transparent.go $pxydir/logger.go
-<<<<<<< HEAD
 mv stintotcp $pxydir/bin/
 mv tcptostdin $pxydir/bin/
 mv $outname $pxydir/bin/
@@ -15,12 +14,3 @@ mv $outname $pxydir/bin/
 # cp -r $pxydir/proxyconfig.json .
 cp -r $pxydir/CV $pxydir/bin/
 sudo chmod -R 770 $pxydir/bin/*
-=======
-mv stintotcp bin/
-mv tcptostdin bin/
-mv $outname bin/
-cp $pxydir/CV/* bin/
-cp $pxydir/proxyconfig.json .
-cp $pxydir/CVhandles.py bin/
-sudo chmod 770 bin/*
->>>>>>> 2d454df8bfabace6cdb6e86b280f14d1ac1ba8c9
