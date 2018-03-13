d771b33c4463f2c3be9d6366c5f9842a08b51a19
anotheroctopus <li1735@purdue.edu>
2018-03-13 11:18:51 -0400

    stuff

--- surface/pakfront/proxyinstall.sh
+++ surface/pakfront/proxyinstall.sh
@@ -2,15 +2,22 @@
 pxydir="pakfront"
 outname="panzerkanone"
 sudo apt-get install -y golang 
+CWD="$(pwd)"
+PROF="$(tail -n 1 ~/.profile)"
+if [[ $PROF != *"GOPATH"* ]]; then
+        echo "Updating Profile"
+        echo "export GOPATH=${CWD}/pakfront/" >> ~/.profile
+        source ~/.profile
+fi
 go get github.com/graarh/golang-socketio
 go get github.com/googollee/go-socket.io
 go build $pxydir/stintotcp.go 
 go build $pxydir/tcptostdin.go 
 go build -o $outname $pxydir/pakfront.go $pxydir/imgbuffer.go $pxydir/sockiopxy.go $pxydir/CVdata.go $pxydir/transparent.go $pxydir/logger.go
-mv stintotcp bin/
-mv tcptostdin bin/
-mv $outname bin/
-cp -r $pxydir/* bin/
-cp -r $pxydir/proxyconfig.json .
-cp $pxydir/CVhandles.py bin/
-sudo chmod -R 770 bin/*
+mv stintotcp $pxydir/bin/
+mv tcptostdin $pxydir/bin/
+mv $outname $pxydir/bin/
+cp $pxydir/CV/* $pxydir/bin/
+cp $pxydir/proxyconfig.json .
+cp $pxydir/CVhandles.py $pxydir/bin/
+sudo chmod -R 770 $pxydir/bin/*
