a28ef5b5e48528cfe02d1c0cd1b0d2dea5c8bfdf
anotheroctopus <li1735@purdue.edu>
2018-03-18 22:13:42 -0400

    transmitter code

--- rov/sensors/axuillarymicros/transmitter/transmitter.ino
+++ rov/sensors/axuillarymicros/transmitter/transmitter.ino
@@ -1,21 +1,55 @@
-#define INPUT_PIN 10
-void onTransmit(){
+#include <avr/io.h>
+#include <util/delay.h>
+
+#define RINP PA7
+#define FINP PB2
+#define PDEL 0.1
+void pulse(){
   
-}
-void offTransmit(){
+  //Idle
+  PORTA &= ~(1 << RINP);
+  PORTB &= ~(1 << FINP);
+  _delay_us(PDEL);
   
+  //Forward
+  PORTA &= ~(1 << RINP);
+  PORTB |= (1 << FINP);
+  _delay_us(PDEL);
+
+  //Idle
+  PORTA &= ~(1 << RINP);
+  PORTB &= ~(1 << FINP);
+  _delay_us(PDEL);
+
+  //Reverse
+  PORTA |= (1 << RINP);
+  PORTB &= ~(1 << FINP);
+  _delay_us(PDEL);
+
+  //Idle
+  PORTA &= ~(1 << RINP);
+  PORTB &= ~(1 << FINP);
+  _delay_us(PDEL);
 }
+
 void setup() {
-   pinMode(4,OUTPUT);  
-   pinMode(INPUT_PIN,INPUT);
+DDRA |= (1 << PA6);
+
+DDRA &= ~(1 << PA3);
+DDRA &= ~(1 << RINP);
+DDRB &= ~(1 << FINP);
+
+PORTA |= (1 << PA3);
+PORTA |= (1 << RINP);
+PORTB |= (1 << FINP);
 }
 
 void loop() {
-if(digitalRead(INPUT_PIN)){
-        digitalWrite(4,HIGH);
-}else{
-        digitalWrite(4,LOW);
-}
-  
+  if (bit_is_clear(PINA,PA3)){
+    pulse();
+    PORTA |= (1 << PA6);
+  }else{
+    PORTA &= ~(1 << PA6);
+  }
 }
 
