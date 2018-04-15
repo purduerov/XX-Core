c5192486db8a29cd7862f7de94d52044fe09a2f7
Ben Maxfield <bmaxfie@purdue.edu>
2018-04-15 21:35:34 +0000

    added tests

--- rov/hardware/servo/Servo.py
+++ rov/hardware/servo/Servo.py
@@ -24,7 +24,7 @@ class Servo(object):
 
         # PWM range
         wiringpi.pwmSetRange(2000)
-        setAngle(0)
+        self.setAngle(0)
 
     def setAngle(self, rawangle):
         # map [-90, 90] to [50, 249]
@@ -52,3 +52,6 @@ class Servo(object):
 
         # Convert the 0-1 range into a value in the right range.
         return int(round(rightMin + (valueScaled * rightSpan)))
+if __name__ == "__main__":
+    s = Servo()
+    s.setAngle(60)
