6db0583b6c0458566d6e4bfd3aea7208e85932ad
anotheroctopus <li1735@purdue.edu>
2018-04-15 19:41:29 -0400

    working communication for atmel

--- rov/sensors/axuillarymicros/current/current.ino
+++ rov/sensors/axuillarymicros/current/current.ino
@@ -5,50 +5,47 @@
  
 //define pin connections
 
-#define CS_PIN1 7
-#define CS_PIN2 8
-#define CS_PIN3 12
-#define CS_PIN4 13
+#define CS_PIN1 14
+#define CS_PIN2 14
+#define CS_PIN3 14
+#define CS_PIN4 14
 //consider using ICSP instead of these pins
 
-#define CLOCK_PIN 9
-#define MOSI_PIN 11
-#define MISO_PIN 10
+#define CLOCK_PIN 17
+#define MOSI_PIN 15
+#define MISO_PIN 16
  
 MCP3008 adc1(CLOCK_PIN, MOSI_PIN, MISO_PIN, CS_PIN1);
 MCP3008 adc2(CLOCK_PIN, MOSI_PIN, MISO_PIN, CS_PIN2);
-MCP3008 adc3(CLOCK_PIN, MOSI_PIN, MISO_PIN, CS_PIN3);
-MCP3008 adc4(CLOCK_PIN, MOSI_PIN, MISO_PIN, CS_PIN4);
-
+int currentdata[16];
+int idx = 0;
+int off = 0;
 void setup() {
   // put your setup code here, to run once:
-  Wire.begin(0x76);
+  Wire.begin(0x2B);
   Wire.onRequest(writeData);
 }
 
 void loop() {
-  
+    for (int i=0; i < 8; i++) {
+    int val1 = adc1.readADC(i);
+    //val1 = off + i;
+    currentdata[i] = val1;
+  }
+
+/*  for (int i = 0; i < 8; i++) {
+    int val2 = adc2.readADC(i);
+    val2 = off + i + 8;
+    currentdata[i+8] = val2;
+  }*/
 }
 
 void writeData() {
-  for (int i=0; i < 8; i++) {
-    int val1 = adc1.readADC(i);
-    Wire.write(val1);
-  }
-
-  for (int i = 0; i < 8; i++) {
-    int val2 = adc2.readADC(i);
-    Wire.write(val2);
-  }
-
-  for (int i = 0; i < 8; i++) {
-    int val3 = adc3.readADC(i);
-    Wire.write(val3);
-  }
-
-  for (int i = 0; i < 8; i++) {
-    int val4 = adc4.readADC(i);
-    Wire.write(val4);
-  }
+      Wire.write(currentdata[idx]);
+      ++idx;
+      if(idx>=16){
+        idx = 0;
+      }
+      off++;
 }
 
