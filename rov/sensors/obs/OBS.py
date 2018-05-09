8878230b4289b5cf3d469e2fc0abd0c25c25598e
anotheroctopus <li1735@purdue.edu>
2018-05-09 11:54:37 -0400

    implemented obs tool

--- rov/sensors/obs/OBS.py
+++ rov/sensors/obs/OBS.py
@@ -1,17 +1,72 @@
+import subprocess
+import requests
+import re
+from time import time
 
+class Status():
+    connected = "connected"
+    unconnected = "unconnected"
+    connecting = "connecting"
+    datanotthrough = "connected,nodata"
+    datathrough = "connected,withdata"
+    level = "connected,OBSisLevel"
+    idkman = "recieveddatabutitsweirdforsomereason"
 
 class OBS(object):
 
     def __init__(self):
         self.x_tilt = 1
         self.y_tilt = 2
-        self.z_tilt = 3
-        self.time = [0, 0.5, 1, 0.5]
+        self.z_tilt = 0
+        self.time = list(range(16))
         self.amplitude = [1, 0.5, 0.34, 0.2, 0.6]
 
+        self.timesconnected = 0
+        self.connectionstostart = 5
+        self.lastconnectattempt = time()
+        self.voltage = 0
+        
+        self.lastupdate = 9999
+        self.status = Status.unconnected
+
     def update(self):
-        pass
+        connecttime = time()
+        if connecttime - self.lastconnectattempt > 0.1:
+                rawips = subprocess.check_output(["hostname","-I"]).strip('\n').split(" ")
+                ips = [ip for ip in rawips if ip]
+                if len(ips) > 1:
+                        self.timesconnected += 1
+                        self.status = Status.connecting
+                else:
+                        self.timesconnected = 0
+                        self.status = Status.unconnected
+                         
+                print ips
     
+                self.lastconnectattempt = connecttime
+        if self.timesconnected > self.connectionstostart:
+                try:
+                        r = requests.get("http://192.168.4.1",timeout = 0.1)
+                except Exception(requests.exceptions.Timeout):
+                        self.status = Status.datanotthrough
+                else:
+                        self.status = Status.datathrough
+                        rawdata = r.text
+                        if "Xangle" in rawdata:
+                                query = "Voltage\s+(?P<volt>-?\d+\.\d+)?.*Xangle=(?P<xangle>-?\d+\.\d+)?.*Yangle=(?P<yangle>-?\d+\.\d+)?.*Count=(?P<count>\d+)"
+                                found = re.find(query,rawdata)
+                                self.voltage = float(found.group("volt"))
+                                self.x_tilt = float(found.group("xangle"))
+                                self.y_tilt = float(found.group("yangle"))
+
+                        elif "DATA" in rawdata: 
+                                query = "DATA:(?P<points>.*)"
+                                found = re.find(query,rawdata)
+                                cleanedfound = m.group("points").strip("\n").replace(" ","").split(",")
+                                self.amplitude = [int(c) for c in cleanedfound]                    
+                        else:
+                                self.status = Status.idkman
+                
     @property
     def data(self):
         return {
@@ -23,5 +78,15 @@ class OBS(object):
             'seismograph_data': {
                 'time': self.time,
                 'amplitude': self.amplitude
-            }
+            },
+            'meta': {
+                'lastupdate': self.lastupdate,
+                'connectstat': self.status,
+                'obsvoltage': self.voltage
+             }
         }
+
+if __name__ == "__main__":
+    obs = OBS()
+    while True:
+            obs.update()
