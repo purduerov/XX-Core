f254e6dce43a727541b7478172a8c3b896a1bcf5
Charles Li <li1735@purdue.edu>
2018-06-21 00:48:38 -0700

    Obs interface (#43)
    * implemented obs tool
    
    * adding update code
    
    * updating packet
    
    * adding comments
    
    * obs tool tested with hotspot
    
    * remove print statements, modify imports
    
    * Ian editing the pull request to show the OBS tool
    
    * oh yeah changing the order

--- rov/sensors/obs/OBS.py
+++ rov/sensors/obs/OBS.py
@@ -1,17 +1,90 @@
+import subprocess
+import requests
+import re
+from time import time
 
+class Status():
+    connected = "connected"
+    unconnected = "unconnected"
+    connecting = "connecting"
+    connectednovoltage = "connectednovoltage"
+    datanotthrough = "connected,nodata"
+    datathrough = "connected,withdata"
+    level = "connected,OBSisLevel"
+    idkman = "recieveddatabutitsweirdforsomereason"
 
 class OBS(object):
 
-    def __init__(self):
+    def __init__(self,ip = "192.168.42.1",connstart=5):
+        self.ip = ip
         self.x_tilt = 1
         self.y_tilt = 2
-        self.z_tilt = 3
-        self.time = [0, 0.5, 1, 0.5]
-        self.amplitude = [1, 0.5, 0.34, 0.2, 0.6]
+        self.z_tilt = 0
+        self.time = list(range(16))
+        self.amplitude = [0 for i in range(16)]
+
+        self.timesconnected = 0 # How many times we have successfully connected
+        self.connectionstostart = connstart # How many successful connection checks in order to begin requesting
+        self.lastconnectattempt = time() # the last time we attempted a connection
+        
+        self.lastupdate = 0 # Last time we recieved correct data
+        self.voltage = 0 # OBS voltage
+        self.status = Status.unconnected
 
     def update(self):
-        pass
+        connecttime = time()
+        if connecttime - self.lastconnectattempt > 0.1:
+                rawips = subprocess.check_output(["hostname","-I"]).strip('\n').split(" ")
+                ips = [ip for ip in rawips if ip]
+                if len(ips) > 1:
+                        self.timesconnected += 1
+                        self.status = Status.connecting
+                else:
+                        self.timesconnected = 0
+                        self.status = Status.unconnected
+                         
     
+                self.lastconnectattempt = connecttime
+        if self.timesconnected > self.connectionstostart:
+                try:
+                        r = requests.get("http://{}".format(self.ip),timeout = 0.1)
+                except:
+                        self.status = Status.datanotthrough
+                else:
+                        self.status = Status.datathrough
+                        rawdata = r.text
+                        if "DATA" in rawdata: 
+                                query = "DATA:(?P<points>.*)"
+                                found = re.search(query,rawdata)
+                                if found != None:
+                                        cleanedfound = found.group("points").strip("\n").replace(" ","").split(",")
+                                        self.amplitude = [int(c) for c in cleanedfound]
+
+                                        self.lastupdate = time()
+                                else:
+                                        self.status = Status.idkman
+                        elif "Xangle" in rawdata:
+                                query = "Voltage=(?P<volt>-?\d+\.\d+)?.*Xangle=(?P<xangle>-?\d+\.\d+)?.*Yangle=(?P<yangle>-?\d+\.\d+)?.*Count=(?P<count>\d+)"
+                                found = re.search(query,rawdata)
+                                if found != None:
+                                        self.voltage = float(found.group("volt"))
+                                        self.x_tilt = float(found.group("xangle"))
+                                        self.y_tilt = float(found.group("yangle"))
+
+                                        self.lastupdate = time()
+                                else:
+                                        self.status = Status.idkman
+                        elif "Voltage" in rawdata:
+                                self.status = Status.connectednovoltage
+                                query = "Voltage=(?P<volt>-?\d+\.\d+)?.*"
+                                found = re.search(query,rawdata)
+                                if found != None:
+                                        self.voltage = float(found.group("volt"))
+                                else:
+                                        self.status = Status.idkman
+                        else:
+                                self.status = Status.idkman
+                
     @property
     def data(self):
         return {
@@ -23,5 +96,15 @@ class OBS(object):
             'seismograph_data': {
                 'time': self.time,
                 'amplitude': self.amplitude
-            }
+            },
+            'meta': {
+                'lastupdate': time() - self.lastupdate,
+                'connectstat': self.status,
+                'obsvoltage': self.voltage
+             }
         }
+
+if __name__ == "__main__":
+    obs = OBS( ip = "192.168.43.176")
+    while True:
+            obs.update()
--- surface/frontend/index.jsx
+++ surface/frontend/index.jsx
@@ -104,6 +104,16 @@ class App extends React.Component {
                     <Card title="Crash Zone Calculator">
                       <CrashZone />
                     </Card>
+                    <Card>
+                      <ToolView manipulator={this.state.dearflask.manipulator.power}
+                                obs_tool={this.state.dearflask.obs_tool.power}
+                                servo={this.state.dearflask.maincam_angle}
+                                transmitter={this.state.dearflask.transmitter}
+                                magnet={this.state.dearflask.magnet}
+                                conf={this.state.config.tool_scales}
+                                rend={this.rendTools}
+                      />
+                    </Card>
                   </div>
                   <div className="data-column">
                     <Card title="Directional Control">
@@ -117,15 +127,8 @@ class App extends React.Component {
                     					scales={this.state.config.thruster_control}
                     					/>
                     </Card>
-                    <Card>
-                      <ToolView manipulator={this.state.dearflask.manipulator.power}
-                                obs_tool={this.state.dearflask.obs_tool.power}
-                                servo={this.state.dearflask.maincam_angle}
-                                transmitter={this.state.dearflask.transmitter}
-                                magnet={this.state.dearflask.magnet}
-                                conf={this.state.config.tool_scales}
-                                rend={this.rendTools}
-                      />
+                    <Card title="OBS">
+                      <ShowObject obj={this.state.dearclient.sensors.obs} />
                     </Card>
                   </div>
                   <div className="data-column">
