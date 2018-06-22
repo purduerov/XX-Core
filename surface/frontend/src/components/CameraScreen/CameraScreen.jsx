d27947c8f8bb89c30df9c47c4c50f7ed7ad43525
sdeghuee <sdeghuee@gmail.com>
2018-06-22 15:14:54 -0400

    without pakfront

--- surface/frontend/src/components/CameraScreen/CameraScreen.jsx
+++ surface/frontend/src/components/CameraScreen/CameraScreen.jsx
@@ -65,9 +65,9 @@ export default class Camera_view extends Component {
         super(props);
 
         this.state = {
-            pxybypass: false,
+            pxybypass: true,
             pakconf:{},
-            numcams:2, //Change to the number of accessable cams. For future growth use pakfornt api
+            numcams:1, //Change to the number of accessable cams. For future growth use pakfornt api
             camscreens: [
                 0,
                 1
